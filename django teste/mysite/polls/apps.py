dd6c6e478776049d8e22c32486cffdd4cd6f36fb
pedroplays2020 <102863935+pedroplays2020@users.noreply.github.com>
2023-01-21 15:21:41 +0000

    atulizado 21-01-2022

--- django teste/mysite/polls/apps.py
+++ django teste/mysite/polls/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'
+
+INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]    
